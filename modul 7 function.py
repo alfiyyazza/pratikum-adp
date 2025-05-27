9b36ccf2e06e30c2456fd2faa44d8d848c8a0b82
alfiyyazza <alfiyyaazzahra030@gmail.com>
2025-05-28 00:05:08 +0700

    Add files via upload

---
+++ modul 7 function.py
@@ -0,0 +1,45 @@
+tarif_listrik = [[1500, 2000],[2500, 3000],[4000, 5000],[5000, 7000]]  #golongan (1-4)
+
+def hitung_tagihan(pemakaian_kwh, golongan_tarif=3):
+    golongan_ke = golongan_tarif - 1  # Menyesuaikan posisi array
+    tarif_awal = tarif_listrik[golongan_ke][0]
+    tarif_lanjutan = tarif_listrik[golongan_ke][1]
+    
+    if pemakaian_kwh <= 100:
+        total_biaya = pemakaian_kwh * tarif_awal
+    else:
+        total_biaya = 100 * tarif_awal + (pemakaian_kwh - 100) * tarif_lanjutan
+    
+    return total_biaya
+
+def program_utama():
+    daftar_pelanggan = []
+
+    jumlah = int(input("Masukkan jumlah pelanggan: "))
+
+    for i in range(1, jumlah + 1):
+        print(f"\nPelanggan ke-{i}")
+        kwh = int(input("Pemakaian listrik (dalam kWh): "))
+        gol_input = input("Masukkan golongan tarif (1-4): ")
+
+        if gol_input == "":
+            daftar_pelanggan.append([kwh])  # tanpa golongan
+        else:
+            golongan = int(gol_input)
+            daftar_pelanggan.append([kwh, golongan])
+
+    print("\n=== Rincian Tagihan Listrik ===")
+    urutan = 1
+    for pelanggan in daftar_pelanggan:
+        if len(pelanggan) == 2:
+            kwh = pelanggan[0]
+            golongan = pelanggan[1]
+        else:
+            kwh = pelanggan[0]
+            golongan = 3  # default jika tidak diinput
+
+        tagihan = hitung_tagihan(kwh, golongan)
+        print(f"Pelanggan {urutan}: {kwh} kWh, Golongan {golongan} => Tagihan: Rp {tagihan:,}")
+        urutan += 1
+
+program_utama()
