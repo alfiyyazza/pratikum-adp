a17f31c35b55edfdbf35c44fdb3f8bdaa665037d
alfiyyazza <alfiyyaazzahra030@gmail.com>
2025-06-10 12:12:28 +0700

    Add files via upload

---
+++ modul 9 animasi.py
@@ -0,0 +1,17 @@
+import time
+import os
+from termcolor import colored, cprint
+
+os.system('cls')
+
+cprint("    " + colored(" ^^^ ", "yellow"))
+
+cprint("    " + colored(" ||| ", "white"))
+
+cprint("   " + colored("       ", "white", "on_magenta"))  
+cprint("   " + colored("       ", "white", "on_blue"))    
+cprint("   " + colored("       ", "white", "on_cyan"))     
+cprint("   " + colored("_______", "white", "on_green"))    
+cprint(colored("\nSelamat Ulang Tahun!", "red"))
+
+time.sleep(10)
